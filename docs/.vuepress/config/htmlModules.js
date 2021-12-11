07b637d6679adfa21d2a233e625eee847127ae7f
AsheOne18 <89166015+AsheOne18@users.noreply.github.com>
2021-12-10 20:22:24 -0500

    更新maven

--- docs/.vuepress/config/htmlModules.js
+++ docs/.vuepress/config/htmlModules.js
@@ -7,10 +7,10 @@ module.exports = {
       font-size: 20px;
       font-weight: bold;
       margin: 0 0 8px 0;
-    ">共产党宣言</p>
+    ">社会主义核心价值观</p>
     <img src="https://raw.githubusercontent.com/AsheOne18/Web-SimpleExample-MusicPlayer/main/img/gcd.png"  style="width:100%;" />
     <p>
-    富强、民主、文明、和谐，自由、平等、公正、法治，爱国、敬业、诚信、友善，是社会主义核心价值观的基本内容。 
+    富强、民主、文明、和谐，自由、平等、公正、法治，爱国、敬业、诚信、友善，是社会主义核心价值观的基本内容。<br> 
     要把培育和践行社会主义核心价值观融入国民教育全过程，落实到经济发展实践和社会治理中。
     </p>
     </div>`,
