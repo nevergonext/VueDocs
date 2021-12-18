c81b0e8405ad0dee133af10325358a91cf6dd181
AsheOne18 <89166015+AsheOne18@users.noreply.github.com>
2021-12-17 22:45:44 -0500

    提交《Spring5》

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -26,6 +26,13 @@ module.exports = [
           { text: '《C++》', link: '/note/cpp/' },
         ],
       },
+      {
+        text: '相关技术',
+        items: [
+          { text: '《Redis》', link: '/note/redis/' },
+          { text: '《Docker》', link: '/note/docker/' },
+        ],
+      },
     ],
   },
   // ------------------------------------------------
@@ -37,12 +44,25 @@ module.exports = [
   text: '学习笔记',
   items: [
     {text: '《Maven》',link: '/note/maven/'},
+    {text: '《MyBatis》',link: '/note/mybatis/'},
     {text: '《Spring5》',link: '/note/spring5/'},
+    {text: '《SpringMVC》',link: '/note/springmvc/'},
+    {text: '《SpringBoot》',link: '/note/springboot/'},
+    {text: '《SpringCloud》',link: '/note/springcloud/'},
   ]
 }
     ],
   },
   // -------------------------------------------------
+  {
+    text: '收藏',
+    link: '/pages/beb6c0bd8a66cea6/',
+    // items: [
+    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+    // ],
+  },
   {
     text: '小记',
     link: '/more/',
@@ -63,15 +83,6 @@ module.exports = [
     ],
   },
 
-  {
-    text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
-    // items: [
-    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-    // ],
-  },
   {
     text: '索引',
     link: '/archives/',
