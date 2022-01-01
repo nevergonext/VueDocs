13cea65634a994cad744f35c1bac79e42528f0e6
AsheOne18 <89166015+AsheOne18@users.noreply.github.com>
2021-12-31 21:39:01 -0500

    1

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -53,14 +53,14 @@ module.exports = [
   },
   // -------------------------------------------------
   {
-    text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
-    // items: [
-    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-    // ],
+    text: '问题',
+    link: '/bug/',
+    items: [
+      { text: '后端Bug', link: '/pages/270ace/' },
+      // { text: '前端Bug'}, link: 'asdsaddsa' },
+    ],
   },
+
   {
     text: '小记',
     link: '/more/',
